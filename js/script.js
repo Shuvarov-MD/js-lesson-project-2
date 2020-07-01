03130c3904eaba0f2706e34627e58770692114d4
Mikhail Shuvarov <shyvarov@gmail.com>
2020-07-01 21:34:12 +0300

    feat: created one event for the menu

--- js/script.js
+++ js/script.js
@@ -44,28 +44,33 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	//Меню
 	const toggleMenu = () => {
-		const btnMenu = document.querySelector('.menu'),
+		const //btnMenu = document.querySelector('.menu'),
 			menu = document.querySelector('menu');
 
 		const handlerMenu = () => {
 			menu.classList.toggle('active-menu');
 		};
 
-		btnMenu.addEventListener('click', handlerMenu);
-		menu.addEventListener('click', event => {
+		document.body.addEventListener('click', event => {
 			let target = event.target;
 
-			if (target.classList.contains('close-btn')) {
+			if (target.closest('.menu')) {
 				handlerMenu();
-			} else {
-				target = target.closest('li');
+			} else if (!target.closest('menu') && menu.classList.contains('active-menu')) {
+				handlerMenu();
+			} else if (target.closest('menu')) {
+				if (target.classList.contains('close-btn')) {
+					handlerMenu();
+				} else {
+					target = target.closest('li');
 
-				//Плавная прокрутка до блока
-				event.preventDefault();
-				const menuLink = target.querySelector('a[href^="#"]'),
-					linkID = menuLink.getAttribute('href');
-				document.querySelector(linkID).scrollIntoView({ behavior: 'smooth', block: 'start' });
-				handlerMenu();
+					//Плавная прокрутка до блока
+					event.preventDefault();
+					const menuLink = target.querySelector('a[href^="#"]'),
+						linkID = menuLink.getAttribute('href');
+					document.querySelector(linkID).scrollIntoView({ behavior: 'smooth', block: 'start' });
+					handlerMenu();
+				}
 			}
 		});
 	};
